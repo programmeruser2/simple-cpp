6e03f458336868a57a101993649733937e016316
repl.it user <replituser@example.com>
2020-10-29 14:11:39 +0000

    Delete unused files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SimpleCpp.hpp"
-
-int main() {
-  var myFile = open("myFile.txt");
-  print(myFile.read());
-  myFile.write("Hello World writing to file!");
-  myFile.close();
-  return 0;
-}
